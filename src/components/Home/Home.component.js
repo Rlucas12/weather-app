1586e1dcff505991dd30ef69fb83449d0deae436
REYNES Lucas <lucasreynes@hotmail.fr>
2017-04-05 12:28:04 +0200

    Add carousel component for left-panel

--- src/components/Home/Home.component.js
+++ src/components/Home/Home.component.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
+import Carousel from 'nuka-carousel';
 import CityComponent from '../City/City.component';
 import StatisticComponent from '../Statistic/Statistic.component';
 import PrevisionComponent from '../Prevision/Prevision.component';
+import './Home.component.css';
 
 class HomeComponent extends Component {
     render() {
         return (
-            <div>
-                <CityComponent />
-                <StatisticComponent />
-                <PrevisionComponent />
+            <div className="home-container">
+                <div className="left-panel">
+                    <Carousel>
+                        <CityComponent />
+                        <CityComponent />
+                        <CityComponent />
+                    </Carousel>
+                </div>
+                <div className="right-panel">
+                    <StatisticComponent />
+                    <PrevisionComponent />
+                </div>
             </div>
         );
     }
