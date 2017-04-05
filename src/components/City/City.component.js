194ce23abc4592ee1969f87c972d345781f8737c
REYNES Lucas <lucasreynes@hotmail.fr>
2017-04-05 14:15:13 +0200

    Add momentJS

--- src/components/City/City.component.js
+++ src/components/City/City.component.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import DateComponent from '../Date/Date.component';
+import Moment from 'moment';
 import './City.component.css';
 
 class CityComponent extends Component {
     render() {
+        const today = Moment();
         return (
             <div className="city-container">
                 <div className="date">
-                    <span className="mounth">April </span>
-                    <span className="day">3, </span>
-                    <span className="year">2017</span>
+                    <span className="mounth">{today.format("MMMM")} </span>
+                    <span className="day">{today.format("DD")}, </span>
+                    <span className="year">{today.format("YYYY")}</span>
                 </div>
                 <DateComponent />
             </div>
